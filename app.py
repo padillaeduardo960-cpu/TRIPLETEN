356bdcc867314fa87a7fed29079ad02b5c3ad920
Mario Padilla <padillaeduardo960@gmail.com>
2025-12-29 19:00:20 -0600

    Corregir conflictos de merge en app.py

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import streamlit as st
 
 st.header('Lanzar una moneda')
 
 st.write('Esta aplicación aún no es funcional. En construcción.')
-=======
-
->>>>>>> 6deede279324cd858045604297626ab0a53ff766
